ad7b6f46dcb9f5e813ad056ea8a92d7ba57b759c
Roger Yu <roger.yu27@gmail.com>
2020-11-18 19:50:53 -0500

    Added tests for 200 and 404 res.

--- test_items/test_main.py
+++ test_items/test_main.py
@@ -1,4 +1,6 @@
 import pytest
+import responses
+import requests
 
 from src.url_checker import UrlChecker
 from src.url_status import UrlStatus
@@ -16,9 +18,37 @@ def url_status():
     return UrlStatus("", "", "")
 
 
-def test_status_code_200(url_checker):
-    result = url_checker.get_url_status_code("https://www.google.ca/", 2.5)
-    assert result.get_status_code() == 200
+def custom_response(link, status_code):
+    """ Setting up mock network response """
+    responses.add(responses.HEAD, link, status=status_code)
+    resp_code = requests.head(link, timeout=2.5).status_code
+    assert resp_code == status_code
+
+
+@pytest.mark.parametrize(
+    "url, expected_code, expected_result",
+    [
+        ("http://google.ca/", 404, "BAD"),
+        ("http://google.ca/", 200, "GOOD"),
+        ("http://google.ca/", 300, "UNKNOWN"),
+        ("http://youtube.com", 404, "BAD"),
+        ("http://testurl.com", 200, "GOOD"),
+        ("http://123456.com", 300, "UNKNOWN"),
+    ],
+)
+@responses.activate
+def test_status_codes(url_checker, url, expected_code, expected_result):
+    custom_response(url, expected_code)
+    result = url_checker.get_url_status_code(url, 2.5)
+    assert result.get_status_code() == expected_code
+    assert result.get_result_name() == expected_result
+
+
+@responses.activate
+def test_status_code_exception(url_checker):
+    with pytest.raises(requests.exceptions.MissingSchema):
+        custom_response("unknown", 0)
+        url_checker.get_url_status_code("unknown", 2.5)
 
 
 def test_status_code_unknown(url_checker):
